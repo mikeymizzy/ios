a5072a86e9ad75bd26dfe5d5874057f33664fd0c
mikeymizzy <matholemike20@gmail.com>
2026-08-15 16:55:27 +0200

    Point app API to admin host

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -15,7 +15,7 @@ class AppConstants {
   static const String googleServerClientId = '491987943015-agln6biv84krpnngdphj87jkko7r9lb8.apps.googleusercontent.com';
   static const String pusherBroadcustUrl = '/api/v1/broadcasting/user-auth';
 
-  static const String baseUrl = 'https://12to12.co.za';
+  static const String baseUrl = 'https://admin.12to12.co.za';
   static const String categoryUri = '/api/v1/categories';
   static const String topCategoriesUri = '/api/v1/categories/top';
   static const String trendingSearchesUri = '/api/v1/trending-searches';
